1713b45d094e43dd29cf1d26a72e2440a571499d
jiaguo <56522923+ChinaVeryNb@users.noreply.github.com>
2020-09-21 23:13:43 +0800

    Add files via upload

--- LinkedList.h
+++ LinkedList.h
@@ -3,35 +3,38 @@
 #include "Node.h"
 #include <iostream>
 template <class T>
-class List {
-	public:
-		List();//1 Constructor
-		~List();//2 Destructor
-		void AddToEnd(T data);//3 create a node containing T data and add it to the front of the list 
-		void AddToFront(T data);//4 create a node containing T data and add it to the end of the list 
-		void AddAtIndex(T data, int index);//5 create a node containing T data and add it to the list at index
-		void RemoveFromFront();//6 Delete first item and return its contents 
-		void RemoveFromEnd();//7 Delete last item and return its contents 
-		void RemoveTheFirst(T data);//8 find first instance of T data and remove it
-		void RemoveAllOf(T data);//9 find each instance of T data and remove it 
-		bool ElementExists(T data);//10 Returns a T/F if element exists in list
-		Node<T> *Find(T data);//11 Look for data in the list, return a pointer to its node 
-		int IndexOf(T data);//12 returns an index of the item in the list (zero-based) 
-		T RetrieveFront();//13 returns the data contained in the first node, does not delete it 
-		T RetrieveEnd();//14 returns the data contained in the last node, does not delete it 
-		T Retrieve(int index);//15 returns the data contained in node # index, does not delete it
-		void PrintList();//16 Loop through each node and print the contents of the Node 
-		void Empty();//17 Empty out the list, delete everything 
-		int Length();//18 How many elements are in the list
-	private:
-		Node<T> *head;
-		Node<T> *tail;
-		int length;
+class List
+{
+public:
+	List();								// 1.Constructor
+	~List();							// 2.Destructor
+	void AddToEnd(T data);				// 3.Create a node containing T data and add it to the front of the list
+	void AddToFront(T data);			// 4.Create a node containing T data and add it to the end of the list
+	void AddAtIndex(T data, int index); // 5.Create a node containing T data and add it to the list at index
+	void RemoveFromFront();				// 6.Delete first item and return its contents
+	void RemoveFromEnd();				// 7.Delete last item and return its contents
+	void RemoveTheFirst(T data);		// 8.Find first instance of T data and remove it
+	void RemoveAllOf(T data);			// 9.Find each instance of T data and remove it
+	bool ElementExists(T data);			// 10.Returns a T/F if element exists in list
+	Node<T> *Find(T data);				// 11.Look for data in the list, return a pointer to its node
+	int IndexOf(T data);				// 12.Returns an index of the item in the list (zero-based)
+	T RetrieveFront();					// 13.Returns the data contained in the first node, does not delete it
+	T RetrieveEnd();					// 14.Returns the data contained in the last node, does not delete it
+	T Retrieve(int index);				// 15.Returns the data contained in node # index, does not delete it
+	void PrintList();					// 16.Loop through each node and print the contents of the Node
+	void Empty();						// 17.Empty out the list, delete everything
+	int Length();						// 18.How many elements are in the list
+
+private:
+	Node<T> *head;
+	Node<T> *tail;
+	int length;
 };
 
 //1 Constructor
 template <typename T>
-List<T>::List() {//初始化两个节点 头和尾
+List<T>::List() //初始化两个节点:头和尾
+{
 	head = new Node<T>;
 	tail = new Node<T>;
 	head->next = tail;
@@ -43,264 +46,339 @@ List<T>::List() {//初始化两个节点 头和尾
 
 //2 Destructor
 template <typename T>
-List<T>::~List() {
-	if(length == 0) {
+List<T>::~List()
+{
+	if (length == 0)
+	{
 		delete head;
 		delete tail;
 		head = nullptr;
 		tail = nullptr;
 		return;
 	}
-	while(head->next != nullptr) {
+	while (head->next != nullptr) // 从头开始删除 Node 指针
+	{
 		Node<T> *temp = head;
 		head = head->next;
-		delete temp;
+		delete temp; // 删除原 head 内存空间
+		temp = nullptr;
 	}
 	delete head;
 	head = nullptr;
 }
 
-//3 create a node containing T data and add it to the front of the list 
+//3 create a node containing T data and add it to the front of the list
 template <typename T>
-void List<T>::AddToFront(T data) {
-	Node<T>* temp = this->head;
-	head->data = data;
+void List<T>::AddToFront(T data)
+{
 	head->prev = new Node<T>;
-	Node<T> *p = head;
-	head = head->prev;
-	head->next = p;
+	Node<T> *firstDataNode = head;
+	head = firstDataNode->prev;
+	head->next = firstDataNode;
 	head->prev = nullptr;
+	firstDataNode->data = data;
 	length++;
 }
 
-//4 create a node containing T data and add it to the end of the list 
+//4 create a node containing T data and add it to the end of the list
 template <typename T>
-void List<T>::AddToEnd(T data) {
-	Node<T>* temp = this->tail;
-	tail->data = data;
+void List<T>::AddToEnd(T data)
+{
 	tail->next = new Node<T>;
-	Node<T> *p = tail;
-	tail = tail->next;
-	tail->prev = p;
+	Node<T> *lastDataNode = tail;
+	tail = lastDataNode->next;
+	tail->prev = lastDataNode;
 	tail->next = nullptr;
+	lastDataNode->data = data;
 	length++;
 }
 
 //5 create a node containing T data and add it to the list at index
 template <typename T>
-void List<T>::AddAtIndex(T data, int index) {
-	if(index >= length) {
-		std::cout<<"index is bigger than length"<<std::endl;
-		return ;
+void List<T>::AddAtIndex(T data, int index)
+{
+	if (index > length)
+	{
+		std::cout << "index is bigger than length" << std::endl;
+		return;
 	}
-	int x = 0;
+	int i = 0;
 	Node<T> *p;
-	p = head->next;
-	while (p->next != nullptr && ++x != index) {
-		p=p->next;
+	p = head;
+	while (p->next != tail && i++ != index)
+	{
+		p = p->next;
 	}
-	Node<T>* temp = p->next;
-	p->next = new Node<T>;
-	p->next->data = data;
-	p->next->prev = p;
-	p->next->next = temp;
-	temp->prev = p->next;
+	Node<T> *thisDataNode = new Node<T>;
+	p->next->prev = thisDataNode;
+	thisDataNode->next = p->next;
+	thisDataNode->prev = p;
+	p->next = thisDataNode;
+	thisDataNode->data = data;
 	length++;
-	return ;
+	return;
 }
 
-//6 Delete first item and return its contents 
+//6 Delete first item
 template <typename T>
-void List<T>::RemoveFromFront() {
-	if(length == 0) {
+void List<T>::RemoveFromFront()
+{
+	if (length == 0)
+	{
 		std::cout << "List is empty";
-		return ;
+		return;
 	}
-	this->head->next->next->prev = this->head;
-	this->head->next = this->head->next->next;
+	head->next->next->prev = head;
+	head->next = head->next->next;
 	length--;
-	return ;
-
+	return;
 }
 
-//7 Delete last item and return its contents 
+//7 Delete last item
 template <typename T>
-void List<T>::RemoveFromEnd() {
-	if(length == 0) {
+void List<T>::RemoveFromEnd()
+{
+	if (length == 0)
+	{
 		std::cout << "List is empty";
-		return ;
+		return;
 	}
-	this->tail->prev->prev->next = this->tail;
-	this->tail->prev = this->tail->prev->prev;
+	tail->prev->prev->next = tail;
+	tail->prev = tail->prev->prev;
 	length--;
-	return ;
-
+	return;
 }
 
 //8 find first instance of T data and remove it
 template <typename T>
-void List<T>::RemoveTheFirst(T data) {
-	if(length == 0) {
+void List<T>::RemoveTheFirst(T data)
+{
+	if (length == 0)
+	{
 		std::cout << "List is empty";
-		return ;
+		return;
 	}
 	Node<T> *p = head;
-	while(p->next!=nullptr) {
+	while (p->next != tail)
+	{
 		p = p->next;
-		if(p->data == data) {
-			Node<T> *temp = p->prev;
-			temp->next = p->next;
-			p->next->prev = temp;
-			delete p;
+		if (p->data == data)
+		{
+			p->prev->next = p->next;
+			p->next->prev = p->prev;
 			length--;
-			return ;
+			return;
 		}
 	}
 }
 
-//9 find each instance of T data and remove it 
+//9 find each instance of T data and remove it
 template <typename T>
-void List<T>::RemoveAllOf(T data) {
-	if(length == 0) {
+void List<T>::RemoveAllOf(T data)
+{
+	if (length == 0)
+	{
 		std::cout << "List is empty";
-		return ;
+		return;
 	}
 	Node<T> *p = head;
-	while(p->next!=nullptr) {
+	while (p->next != tail)
+	{
 		p = p->next;
-		if(p->data == data) {
-			Node<T> *temp = p->prev;
-			temp->next = p->next;
-			p->next->prev = temp;
+		if (p->data == data)
+		{
+			p->prev->next = p->next;
+			p->next->prev = p->prev;
 			length--;
 		}
 	}
-	return ;
+	return;
 }
 
 //10 Returns a T/F if element exists in list
 template <typename T>
-bool List<T>::ElementExists(T data) {
-	if(length == 0) {
+bool List<T>::ElementExists(T data)
+{
+	if (length == 0)
+	{
 		std::cout << "List is empty" << std::endl;
 		return false;
 	}
-	Node<T> *p = head->next;
+	Node<T> *p = head;
 	bool flag = false;
-	while (p != tail) {
-		if(p->data == data){
-			flag = true;
-		}
+	while (p->next != tail)
+	{
 		p = p->next;
+		if (p->data == data)
+		{
+			flag = true;
+			break;
+		}
 	}
-	if(flag){
+	if (flag)
+	{
 		return true;
-	} else {
+	}
+	else
+	{
 		return false;
 	}
-}
-;
+};
 
 //11 Look for data in the list, return a pointer to its node
 template <typename T>
-Node<T>* List<T>::Find(T data) {
-	if(length == 0) {
+Node<T> *List<T>::Find(T data)
+{
+	if (length == 0)
+	{
 		std::cout << "List is empty" << std::endl;
 		return nullptr;
 	}
-	Node<T> *p = head->next;
-	while (p != tail) {
-		if(p->data == data){
-			return p;
-		}
+	Node<T> *p = head;
+	bool flag = false;
+	while (p->next != tail)
+	{
 		p = p->next;
+		if (p->data == data)
+		{
+			flag = true;
+			break;
+		}
 	}
-	return nullptr;
+	if (flag)
+	{
+		return p;
+	}
+	else
+	{
+		return nullptr;
+	}
 }
 
-//12 returns an index of the item in the list (zero-based) 
+//12 returns an index of the item in the list (zero-based)
 template <typename T>
-int List<T>::IndexOf(T data) {
-	if(length == 0) {
+int List<T>::IndexOf(T data)
+{
+	if (length == 0)
+	{
 		std::cout << "List is empty" << std::endl;
 		return -1;
 	}
-	int x = 0;
-	Node<T> *p = head->next;
-	while (p != tail) {
-		if(p->data == data){
-			return x;
-		}
-		x++;
+	int i = 0;
+	Node<T> *p = head;
+	bool flag = false;
+	while (p->next != tail)
+	{
 		p = p->next;
+		if (p->data == data)
+		{
+			flag = true;
+			break;
+		}
+		i++;
 	}
-	return -1;
+	if (flag)
+	{
+		return i;
+	}
+	else
+	{
+		return -1;
+	}
 }
 
-//13 returns the data contained in the first node, does not delete it 
+//13 returns the data contained in the first node, does not delete it
 template <typename T>
-T List<T>::RetrieveFront() {
+T List<T>::RetrieveFront()
+{
+	if (length == 0)
+	{
+		std::cout << "List is empty" << std::endl;
+		return T(NULL);
+	}
 	return head->next->data;
 }
 
-//14 returns the data contained in the last node, does not delete it 
+//14 returns the data contained in the last node, does not delete it
 template <typename T>
-T List<T>::RetrieveEnd() {
+T List<T>::RetrieveEnd()
+{
+	if (length == 0)
+	{
+		std::cout << "List is empty" << std::endl;
+		return T(NULL);
+	}
 	return tail->prev->data;
 }
 
-//15 returns the data contained in node # index, does not delete it
+//15 returns the data contained in node #index, does not delete it
 template <typename T>
-T List<T>::Retrieve(int index) {
-	if(length == 0) {
-		return NULL;
+T List<T>::Retrieve(int index)
+{
+	if (length == 0)
+	{
+		std::cout << "List is empty" << std::endl;
+		return T(NULL);
 	}
-	if(index >= length) {
-		return NULL;
+	if (index >= length)
+	{
+		std::cout << "index is bigger than length" << std::endl;
+		return T(NULL);
 	}
-	int x = 0;
-	T data;
+	int i = 0;
 	Node<T> *p;
-	if(index < length/2) {
+	if (index < length / 2)
+	{
 		p = head->next;
-		while (p->next != nullptr && x++ != index) {
-			p=p->next;
+		while (p->next != tail && i++ != index)
+		{
+			p = p->next;
 		}
-	} else {
+	}
+	else
+	{
 		p = tail->prev;
-		while(p->prev != nullptr && x++ != index) {
+		while (p->prev != head && i++ != length - 1 - index)
+		{
 			p = p->prev;
 		}
 	}
 	return p->data;
 }
 
-//16 Loop through each node and print the contents of the Node 
+//16 Loop through each node and print the contents of the Node
 template <typename T>
-void List<T>::PrintList() {
-	if(length == 0) {
+void List<T>::PrintList()
+{
+	if (length == 0)
+	{
 		std::cout << "List is empty" << std::endl;
 		return;
 	}
-	Node<T> *p = head->next;
-	while (p != tail) {
-		std::cout << p->data << " ";
+	Node<T> *p = head;
+	while (p->next != tail)
+	{
 		p = p->next;
+		std::cout << p->data << " ";	
 	}
 	std::cout << std::endl;
 }
 
-//17 Empty out the list, delete everything 
+//17 Empty out the list, delete everything
 template <typename T>
-void List<T>:: Empty() {
-	if(length == 0) {
-		return ;
+void List<T>::Empty()
+{
+	if (length == 0)
+	{
+		return;
 	}
 	Node<T> *p = head->next;
-	while(p != tail) {
-		Node<T>* temp = p;
+	while (p->next != tail)
+	{
+		Node<T> *temp = p;
 		p = p->next;
 		delete temp;
+		temp = nullptr;
 	}
 	head->next = tail;
 	tail->prev = head;
@@ -309,7 +387,8 @@ void List<T>:: Empty() {
 
 //18 How many elements are in the list
 template <typename T>
-int List<T>::Length() {
+int List<T>::Length()
+{
 	return length;
 }
 
